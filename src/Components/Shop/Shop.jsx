6f49eea382129a97f8ae809de6ce61f600778630
Sakib360360 <saakibabrar@gmail.com>
2023-10-09 09:42:58 +0600

    shop card

--- src/Components/Shop/Shop.jsx
+++ src/Components/Shop/Shop.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 const Shop = ({item}) => {
     console.log(item)
-    const {details,name,price,picture,sellerName,_id} = item
+    const {details,name,price,picture,} = item
     return (
         <div className=''>
             <div data-aos="fade-up"
                 data-aos-anchor-placement="center-bottom">
-                <div className="card w-80 h-96 md:w-60 bg-base-100 shadow-sm border-2 ">
+                <div className="card w-96 h-96 md:w-60 bg-base-100 shadow-sm border-2 ">
                     <figure><img className='h-40 rounded-t-2xl w-full' src={picture} alt="photo of product" /></figure>
                     <hr />
                     <div className="card-body h-52 bg-slate-100">
